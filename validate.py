98784f294322f4e4f9154f13880d27e722b1d009
Soichi Hayashi <hayashis@iu.edu>
2020-04-27 17:56:54 +0000

    added code to output x/y/z snapshots of all rois

--- validate.py
+++ validate.py
@@ -2,6 +2,14 @@
 
 import os
 import json
+import nibabel as nib
+import numpy as np
+from PIL import Image
+import sys
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 with open('config.json', encoding='utf8') as config_json:
     config = json.load(config_json)
@@ -12,10 +20,44 @@ meta = {}
 
 #enumerate all nifties
 meta["rois"] = []
+merged_img = None
+shape = None
+count = 0
 for file in os.listdir(config["rois"]):
     #TODO - maybe check to make sure all rois are same size?
-    meta["rois"].append(file)
+    print(file)
+    img = nib.load(config["rois"]+"/"+file)
+    img_data = img.get_fdata()
+
+    count += 1
+
+    if shape == None:
+        #first image
+        shape = img_data.shape
+        meta["shape"] = shape
+        print(shape)
+        merged_img = img_data
+
+    else:
+        #following images
+        if shape != img_data.shape:
+            print("not the same shape")
+        else:
+            print("shape looks good")
+
+        #merged_img[merged_img<1] = img_data
+        merged_img[img_data==1] = count #replace with roi specific value
+        #merged_img = np.where(img_data>0, img_data, merged_img)
+
+    meta["rois"].append({"filename": file, "voxels": np.count_nonzero(img_data)})
+
+#np.set_printoptions(threshold=sys.maxsize)
+#print("max merged")
+#print(np.min(merged_img))
+#print(np.max(merged_img))
+
 meta["roicount"] = len(meta["rois"])
+#print(merged_img)
 
 #setup outout
 if not os.path.exists("output"):
@@ -28,7 +70,23 @@ if not os.path.exists("output"):
 if not os.path.exists("secondary"):
     os.mkdir("secondary")
 
-#TODO - let's create a 3-axis views of all rois superimposed (to show the relationship)
+#normalize image between 0-255
+merged_img /= count #normalize to 0-1
+merged_img *= 255
+
+#let's create a 3-axis views of all rois superimposed (to show the relationship)
+img_x = np.max(merged_img, axis=0)
+img_y = np.max(merged_img, axis=1)
+img_z = np.max(merged_img, axis=2)
+
+ix = Image.fromarray(np.flipud(img_x))
+ix.convert('L').save("secondary/x.png")
+
+iy = Image.fromarray(np.flipud(img_y))
+iy.convert('L').save("secondary/y.jpg")
+
+iz = Image.fromarray(np.flipud(img_z))
+iz.convert('L').save("secondary/z.jpg")
 
 with open("product.json", "w") as fp:
     json.dump({"errors": errors, "warnings": warnings, "meta": meta}, fp)
